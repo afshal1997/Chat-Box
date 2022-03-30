371d4e2cff08730f5915d928ee95af3fd528e63b
abdulbasit001 <abdul.basit@shottechnologies.co>
2022-03-31 03:18:07 +0500

    updated code

--- Assets/Js/customJs.js
+++ Assets/Js/customJs.js
@@ -1,9 +1,11 @@
+const dummyMessage = document.createElement('div')
+let file_selected;
 function createHiddenInputForFile(parent) {
     const input_type_file = document.createElement('input')
     input_type_file.setAttribute('type', 'file')
     input_type_file.setAttribute('hidden', true)
     input_type_file.setAttribute('id', "select-file")
-    // input_type_file.setAttribute('multiple', true)
+    input_type_file.setAttribute('accept', "image/*")
     input_type_file.setAttribute('onchange', 'selectFiles(event)')
     parent.appendChild(input_type_file)
 }
@@ -14,6 +16,7 @@ function sendMessageAndAction(parent) {
     _input_group.setAttribute('type', 'text')
     _input_group.setAttribute('class', 'form-control rounded-pill')
     _input_group.setAttribute('id', 'chat-input')
+    _input_group.setAttribute('value', '')
     _input_group.setAttribute('placeholder', 'Enter your message')
     _input_group.setAttribute('aria-label', 'Enter-your-message')
     _input_group.setAttribute('aria-describedby', 'basic-addon-to-send-message')
@@ -40,23 +43,32 @@ function openFileBox() {
 function chatAppHeader(parent) {
     const chat_app_header = document.createElement('div')
     chat_app_header.setAttribute('class', 'chat-app_header')
-    chat_app_header.innerHTML = '<div class="close" onclick="toggleChatMode()"></div><div class="branding"><div class="avatar is-online"><img src="Assets/Images/avatar.png" alt="Assets/Images/avatar.png"> </div><div class="content"><p class="title">Nabeel Baig</p><p class="subtitle">Ask me anything, I am here to help you.</p></div></div>'
+    chat_app_header.innerHTML = '<div class="close" onclick="toggleChatMode()"></div><div class="branding"><div class="avatar is-online"><img src="Assets/Images/avatar.png" alt="Assets/Images/avatar.png"> </div><div class="content"><p class="title">User Name</p><p class="subtitle">Ask me anything, I am here to help you.</p></div></div>'
     parent.appendChild(chat_app_header)
 }
-function selectFiles(event) {
-    console.log(event.target.file)
+function selectFiles({ target: { files } }) {
+    document.querySelector('#select-file-name').innerText = files[0].name
+    file_selected = files[0]
+}
+function downloadFile(event) {
+    console.log(event.target)
+}
+function createMessage(message, messageClassName, type = null) {
+    const dummyMessageReply = document.createElement('div')
+    dummyMessageReply.setAttribute('class', `message ${messageClassName}`)
+    if (type) {
+        dummyMessageReply.setAttribute('data-src-attribute', file_selected)
+        dummyMessageReply.setAttribute('onclick', 'downloadFile(event)')
+    }
+    dummyMessageReply.innerHTML = message
+    dummyMessage.appendChild(dummyMessageReply)
 }
 function messagesUI(parent) {
-    const dummyMessageContainer = document.createElement('div')
-    dummyMessageContainer.setAttribute('class', 'chat-app_content')
-    const dummyMessage = document.createElement('div')
     dummyMessage.setAttribute('class', 'messages')
     dummyMessage.setAttribute('id', 'message')
-    const dummyMessageReply = document.createElement('div')
-    dummyMessageReply.setAttribute('class', 'message reply')
-    dummyMessageReply.innerHTML = "reply"
-    dummyMessage.appendChild(dummyMessageReply)
-
+    const dummyMessageContainer = document.createElement('div')
+    dummyMessageContainer.setAttribute('class', 'chat-app_content')
+    createMessage("reply message text", "reply")
     dummyMessageContainer.appendChild(dummyMessage)
     parent.append(dummyMessageContainer)
 }
@@ -97,6 +109,20 @@ function toggleToaster() {
         document.querySelectorAll('.notificationBoxH6')[0].classList.toggle('d-none')
     }, 2000);
 }
+function chat_attachments(parent) {
+    const file_select_button = document.createElement('div')
+    file_select_button.setAttribute('class', 'file-select-button')
+    const fa_fa_paperclip_attachment = document.createElement('i')
+    const select_files_name = document.createElement('span')
+    select_files_name.setAttribute('class', 'text-danger mx-3')
+    select_files_name.setAttribute('id', 'select-file-name')
+    fa_fa_paperclip_attachment.setAttribute('class', 'fa fa-paperclip attachment')
+    fa_fa_paperclip_attachment.setAttribute('aria-hidden', 'true')
+    fa_fa_paperclip_attachment.setAttribute('onclick', 'openFileBox()')
+    file_select_button.appendChild(fa_fa_paperclip_attachment)
+    file_select_button.appendChild(select_files_name)
+    parent.appendChild(file_select_button)
+}
 function chatInit() {
     enableChatAppIcon()
     toggleToaster()
@@ -111,36 +137,30 @@ function chatInit() {
     const chat_app_footer = document.createElement('div')
     chat_app_footer.style.bottom = "0px";
     chat_app_footer.setAttribute('class', "chat-app_footer position-absolute w-100")
+    createHiddenInputForFile(chat_app_footer)
+    chat_attachments(chat_app_footer)
     const file_upload = document.createElement('div')
     file_upload.setAttribute('class', 'file-upload')
     const file_upload_select = document.createElement('div')
     file_upload_select.setAttribute('click', 'file-upload-select')
-    const file_select_button = document.createElement('div')
-    file_select_button.setAttribute('class', 'file-select-button')
-    const fa_fa_paperclip_attachment = document.createElement('i')
-    createHiddenInputForFile(chat_app_footer)
-    fa_fa_paperclip_attachment.setAttribute('class', 'fa fa-paperclip attachment')
-    fa_fa_paperclip_attachment.setAttribute('aria-hidden', 'true')
-    fa_fa_paperclip_attachment.setAttribute('onclick', 'openFileBox()')
-    file_select_button.appendChild(fa_fa_paperclip_attachment)
     sendMessageAndAction(chat_app_footer)
     chat_app_box.appendChild(chat_app_footer);
     document.body.insertAdjacentElement('beforeend', chat_app_box)
 
 }
-
-
-function sendMessage(e) {
-    let formValue = document.getElementById("chat-input").value
+function sendMessage() {
+    let formValue = document.querySelector('#chat-input').value
+    if (file_selected) {
+        createMessage(file_selected.name, 'user-message', 'file')
+        document.querySelector('#select-file-name').innerText = ""
+        file_selected = undefined
+    }
     if (!formValue) {
         return;
     }
-    // let parentChatContainer = document.getElementById('message')
-    // let newMessage = document.createElement("p")
-    // newMessage.setAttribute('class', 'message')
-    // newMessage.innerHTML = formValue
-    // parentChatContainer.appendChild(newMessage)
-    // document.getElementById("chat-input").value = ""
+    createMessage(formValue, 'user-message')
+    document.querySelector('#chat-input').value = ""
+
 }
 
 chatInit()
